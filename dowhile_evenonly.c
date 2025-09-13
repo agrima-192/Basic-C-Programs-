748e2d7842e1da1f4d02431d0913f50d6147c49b
Agrima Khandelwal <agrima19feb@gmail.com>
2025-09-14 00:00:00 +0530

    Add files via upload

---
+++ dowhile_evenonly.c
@@ -0,0 +1,11 @@
+// program to print even no.s only using do...while loop
+#include<stdio.h>
+int main(){
+    int i =2;
+    do {
+        printf("%d \n",i);
+        i=i+2;
+    }
+    while(i<=100);
+    return 0;
+} 
---
+++ dowhile_oddonly.c
@@ -0,0 +1,11 @@
+// program to print odd no.s only using do...while loop
+#include<stdio.h>
+int main(){
+    int i =1;
+    do {
+        printf("%d \n",i);
+        i=i+2;
+    }
+    while(i<=100);
+    return 0;
+} 
