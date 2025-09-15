9d70f35e8a26e6f51995296a23196f8daa52b458
Agrima Khandelwal <agrima19feb@gmail.com>
2025-09-15 23:47:38 +0530

    Add files via upload

---
+++ dowhile_loop1.c
@@ -0,0 +1,12 @@
+// program to print nos between 1 to 100 using do...while loop
+#include<stdio.h>
+int main(){
+    int i =1;
+    do {
+        printf("%d \n",i);
+        i++;
+    }
+    while(i<=100);
+    return 0;
+} 
+
