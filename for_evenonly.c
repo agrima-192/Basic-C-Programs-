81bcdd0bf103c41be24ba6b485cecbd31342f899
Agrima Khandelwal <agrima19feb@gmail.com>
2025-09-15 23:48:14 +0530

    Add files via upload

---
+++ for_evenonly.c
@@ -0,0 +1,9 @@
+// program to print even no.s only using for loop
+#include<stdio.h>
+int main(){
+    for(int i=1;i<=100; i++){
+            if(i%2==0) //evenonly
+            printf("%d \n",i);
+    }
+return 0;
+} 
