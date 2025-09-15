2a9536a9bfe26139ba589b3cc59545d648f92249
Agrima Khandelwal <agrima19feb@gmail.com>
2025-09-15 23:51:38 +0530

    Add files via upload

---
+++ while_oddonly.c
@@ -0,0 +1,11 @@
+// program to print odd no.s only using while loop
+#include<stdio.h>
+int main(){
+    int i =1;
+    while(i<=100)
+    { if(i%2!=0) //odd
+    printf("%d \n",i);
+    i++;
+    }
+    return 0;
+} 
