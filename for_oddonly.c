267489258cc80cb7bebc726e97a120a8f432b573
Agrima Khandelwal <agrima19feb@gmail.com>
2025-09-15 23:49:10 +0530

    Add files via upload

---
+++ for_oddonly.c
@@ -0,0 +1,9 @@
+// program to print odd no.s only using for loop
+#include<stdio.h>
+int main(){
+    for(int i=1;i<=100; i++){
+            if(i%2!=0) //odd only
+            printf("%d \n",i);
+    }
+return 0;
+} 
